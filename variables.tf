6b3e3e99352a48a628886472c534e509a7aabdaf
jpdoherty90 <jpdoherty90@gmail.com>
2021-12-01 23:42:53 -0500

    Added vars

---
+++ variables.tf
@@ -0,0 +1,16 @@
+
+# Input variable definitions
+
+variable "aws_region" {
+  description = "AWS region for all resources."
+
+  type    = string
+  default = "us-east-2"
+}
+
+variable "api_token" {
+  description = "FinnHub API Token for accessing stock data"
+
+  type    = string
+  default = ""
+}
